76ddaf45b429a5b2854c8145db7a210c9bef4426
Laureano <laureanomansilla@neuronic.com.ar>
2024-11-30 16:21:29 -0300

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'jnujnmneeokpkqpghcoj.supabase.co',
+        hostname: 'qfbrqgdzhcnvmqggsbsw.supabase.co',
         pathname: '/**', // Permitir cualquier ruta
       },
     ],
